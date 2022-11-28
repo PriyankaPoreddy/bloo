751708cce25dac205f126acba8f875de4fbfc376
PriyankaPoreddy <priyanka.poreddy2203@gmail.com>
2022-11-28 11:27:37 +0530

    Sprint5 Changes

---
+++ app/src/main/java/uk/ac/tees/w9585141/blooplus/Users.java
@@ -0,0 +1,57 @@
+package uk.ac.tees.w9585141.blooplus;
+
+public class Users {
+
+
+    String Uid;
+    String f_name;
+    String l_name;
+    String ph_num;
+    String email_id;
+
+    public Users(){
+
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getPh_num() {
+        return ph_num;
+    }
+
+    public void setPh_num(String ph_num) {
+        this.ph_num = ph_num;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+
+}
---
+++ app/src/main/java/uk/ac/tees/w9585141/blooplus/splashScreen.java
@@ -0,0 +1,23 @@
+package uk.ac.tees.w9585141.blooplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        }, 3000);
+    }
+}
