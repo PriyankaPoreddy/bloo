af1097b985cd64f53803887181de90298c70cf36
PriyankaPoreddy <priyanka.poreddy2203@gmail.com>
2022-12-10 19:19:46 +0530

    bug fixes-2

---
+++ app/src/main/java/uk/ac/tees/w9585141/blooplus/userImages.java
@@ -0,0 +1,22 @@
+package uk.ac.tees.w9585141.blooplus;
+
+public class userImages {
+
+    String url;
+
+    public userImages(){
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+
+}
